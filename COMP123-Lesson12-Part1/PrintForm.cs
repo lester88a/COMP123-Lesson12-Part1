c1e0136c3b76e9671dbbad7663fab9248e1575c5
lester88a <lester88a@gmail.com>
2015-03-31 12:54:42 -0400

    added print form and print button

--- COMP123-Lesson12-Part1/PrintForm.cs
+++ COMP123-Lesson12-Part1/PrintForm.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace COMP123_Lesson12_Part1
 {
-    public partial class Form1 : Form
+    public partial class PrintForm : Form
     {
-        public Form1()
+        public PrintForm()
         {
             InitializeComponent();
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            printForm1.Print();
         }
     }
 }
--- COMP123-Lesson12-Part1/Program.cs
+++ COMP123-Lesson12-Part1/Program.cs
@@ -16,7 +16,7 @@ namespace COMP123_Lesson12_Part1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PrintForm());
         }
     }
 }
